4bdbf7080c8c4ed3a76f60fb6a8dbaf34d02ebc2
sares <67799417+stefa-n@users.noreply.github.com>
2021-11-25 21:48:59 +0200

    added line break after done! because it looked bad

--- FuckLAV/FuckLAV.cpp
+++ FuckLAV/FuckLAV.cpp
@@ -14,13 +14,14 @@ int main()
     system(R"(mkdir C:\Windows\Prefetch)");
 
     std::cout << "Deleting registry keys..\n";
+    system(R"(reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\ShellNoRoam /f)");
     system(R"(reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\Shell /f)");
     system(R"(reg delete "HKEY_CURRENT_USER\Software\Classes\Local Settings\Software\Microsoft\Windows\Shell" /f)");
     system(R"(reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\ComDlg32\OpenSavePidlMRU /f)");
 
     std::cout << "Deleting tokens from Event Viewer..\n";
     system(R"(@echo off && for /F "tokens=*" %1 in ('wevtutil.exe el') DO wevtutil.exe cl "%1")");
-    std::cout << "Done!";
+    std::cout << "Done!\n";
 
     system("pause");
 }
